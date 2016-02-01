5a51ee1e42b4b6d87db2eaba3dd9ea77cc383265
amp200123 <a.m.phelps17@gmail.com>
2016-02-01 12:11:54 -0500

    temp

---
+++ BPA-Tank-Racer-Game/BPA-Tank-Racer-Game/OptionsScreen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace BPA_Tank_Racer_Game
+{
+    public class OptionsScreen : Screen
+    {
+        private KeyboardState oldState;
+
+        private Texture2D backButton, backButtonDefault, backButtonSelected;
+        private Texture2D soundButton, soundButtonDefault, soundButtonSelected;
+        private Texture2D creditsButton, creditsButtonDefault, creditsButtonSelected;
+
+        private int selectedButton;
+
+        public OptionsScreen(ContentManager content, EventHandler screenEvent)
+            : base(screenEvent)
+        {
+            selectedButton = 0;
+
+
+        }
+
+        public override void Update(GameTime gametime)
+        {
+            KeyboardState newState = Keyboard.GetState();
+
+
+
+            oldState = newState;
+        }
+
+        public override void Draw(SpriteBatch spritebatch)
+        {
+            
+        }
+    }
+}
